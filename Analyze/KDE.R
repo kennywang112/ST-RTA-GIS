8da2874043076d856e16b164479391869a916f00
Wang chi-chien <kennywang2003@gmail.com>
2025-10-16 21:50:26 +0100

    KDE

--- Analyze/KDE.R
+++ Analyze/KDE.R
@@ -6,14 +6,19 @@ library(raster)
 library(adehabitatHR)
 library(tidyverse)
 
-A_p  <- st_transform(A2, 3826)%>%as("Spatial")
-kde.output <- kernelUD(A_p, h="href", grid = 2000)
+data <- read.csv('/Users/wangqiqian/Desktop/ST-RTA/ComputedData/Accident/combineddata_with_CC.csv')
+data <- data%>%filter(COUNTYNAME=='臺北市' & `當事者區分.類別.子類別名稱.車種`=='腳踏自行車')
+A <- st_as_sf(data, coords = c("經度", "緯度"), crs = 4326)%>%st_transform(3826)
+A <- st_zm(A, drop = TRUE, what = "ZM")
+A_sp <- as(A, "Spatial")
 
-plot(kde.output)
+A_sp$ID <- factor("all")
 
-kde <- raster(kde.output)
+kde <- kernelUD(A_sp["ID"], h = "href", grid = 500)
 
-r <- raster(kde.output)
+plot(kde$all)
+
+r <- raster(kde$all)
 
 # 存成 GeoTIFF，QGIS 可直接讀
-writeRaster(r, "../CalculatedData/kde_output.tif", format = "GTiff", overwrite = TRUE)
+writeRaster(r, "./CalculatedData/kde_output_bike.tif", format = "GTiff", overwrite = TRUE)
